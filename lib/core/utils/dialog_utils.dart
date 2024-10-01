4e58e8052b7a59aafe71d4038aac7b432cb945a5
mo_Firas <mohammedfirasaljallad@gamil.com>
2024-10-01 22:36:03 +0300

    +Dialog customization

---
+++ lib/core/utils/dialog_utils.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class DialogUtils {
+  static void ShowLaodingDialog(BuildContext context, String message) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Row(
+              children: [
+                Text(
+                  message,
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  width: 6,
+                ),
+                CircularProgressIndicator()
+              ],
+            ),
+          );
+        });
+  }
+
+  static void hideDialog(BuildContext context) {
+    Navigator.pop(context);
+  }
+
+  static void showMessageDialog(BuildContext context,
+      {String? message,
+      String? posActionTitle,
+      String? negActionTitle,
+      VoidCallback? posAction,
+      VoidCallback? negAction}) {
+    List<Widget> actions = [];
+    if (posActionTitle != null) {
+      actions.add(TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+            posAction?.call();
+          },
+          child: Text(
+            posActionTitle,
+            style: TextStyle(
+              fontSize: 12,
+              color: Colors.black,
+            ),
+          )));
+    }
+
+    if (negActionTitle != null) {
+      actions.add(TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+            negAction?.call();
+          },
+          child: Text(negActionTitle,
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.black,
+              ))));
+    }
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(message ?? '',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black,
+                    )),
+              ],
+            ),
+            actions: actions);
+      },
+    );
+  }
+}
